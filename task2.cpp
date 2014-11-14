1250543e19b13ff50ba4bc292cdbe810fc5dbc76
AlmazNasibullin <nasibyllin.almaz@phystech.edu>
2014-11-14 10:55:44 +0300

    Fixed

--- task2.cpp
+++ task2.cpp
@@ -3,73 +3,83 @@
 #include <algorithm>
 #include <list>
 
-class Cut
+class Segment
 {
 public:
-    Cut();
-    Cut(int start, int length, bool free);
+    Segment();
+    Segment(int start, int length, int indexInHeap, bool free);
     int getLength() const;
     int getStart() const;
-    int isFree() const;
+    int getIndex() const;
+    void setIndex();
+    bool isFree() const;
     void makeFree();
 private:
     int start_;
     int length_;
+    int indexInHeap_;
     bool free_;
 };
 
-std::vector<int> readInputData(int* memorySize, int* numberOfQueries);
+std::vector<int> readQueries(int* memorySize);
 
-// is used to store index in the heap of free cut
-struct CutAndIndex
-{
-    CutAndIndex(Cut cut, int index);
-    Cut cut_;
-    int index_;
-};
-
-// compares two free cuts on length
-// if length of first free cut is less than length of second returns true
-// if length of first free cut is more than length of second returns false
-// otherwise similarly compares starts
-bool compareFreeCutsOnLength(const std::list<CutAndIndex>::iterator firstFreeCut,
-    const std::list<CutAndIndex>::iterator& secondFreeCut);
-
-class HeapOfFreeCuts
+template <class T, class CallBack, class Comparator>
+class Heap
 {
 public:
-    HeapOfFreeCuts();
-    void swap(int firstIndex, int secondIndex);
-    void increaseKey(int index, std::list<CutAndIndex>::iterator key);
-    void insert(std::list<CutAndIndex>::iterator key);
+    Heap();
+    void insert(const T& key);
     void erase(int index);
-    void heapify(int index);
-    std::list<CutAndIndex>::iterator extractMax();
-    std::list<CutAndIndex>::iterator max();
+    T& extractMax();
+    T getMax() const;
     int getHeapSize() const;
     bool empty() const;
 private:
-    std::vector<std::list<CutAndIndex>::iterator> freeCuts;
-    int heapSize;
+    std::vector<T> elements;
+    CallBack callBack;
+
+    void swap(int firstIndex, int secondIndex);
+    void heapify(int index);
 };
 
-void allocateMemory(const std::vector<int>& queries, int queryNumber, HeapOfFreeCuts& freeCuts,
-    std::vector<int>& answers, std::vector<std::list<CutAndIndex>::iterator>& infoOfAllocatedQueries,
-    std::vector<bool>& isAllocated);
+class CallBackForSegments
+{
+public:
+    void operator()(Segment& segment, int newIndexInHeap);
+};
 
-void freeMemory(const std::vector<int>& queries, int queryNumber, HeapOfFreeCuts& freeCuts,
-    std::vector<int>& answers, std::vector<std::list<CutAndIndex>::iterator>& infoOfAllocatedQueries,
-    std::vector<bool>& isAllocated);
+// compares two free segments on length
+// if length of first free segment is less than length of second returns true
+// if length of first free segment is more than length of second returns false
+// otherwise similarly compares starts
+class ComparatorForSegments
+{
+public:
+    bool operator()(const std::list<Segment>::iterator& first,
+    const std::list<Segment>::iterator& second);
+};
+
+class MemoryManager
+{
+public:
+    MemoryManager(int memorySize);
+    int allocateMemory(int query);
+    int freeMemory(int query);
+private:
+    std::list<Segment> allSegments;
+    Heap<std::list<Segment>::iterator, CallBackForSegments, ComparatorForSegments> freeSegments;
+    std::vector<std::list<Segment>::iterator> infoOfAllocatedQueries;
+    std::vector<bool> isAllocated;
+};
 
 std::vector<int> parseQueriesForAllocationOrFreeMemory(const std::vector<int>& queries,
-    int memorySize, int numberOfQueries);
+    int memorySize);
 
 int main()
 {
-    int memorySize, numberOfQueries;
-    std::vector<int> queries = readInputData(&memorySize, &numberOfQueries);
-    std::vector<int> answers = parseQueriesForAllocationOrFreeMemory(queries, memorySize,
-        numberOfQueries);
+    int memorySize;
+    std::vector<int> queries = readQueries(&memorySize);
+    std::vector<int> answers = parseQueriesForAllocationOrFreeMemory(queries, memorySize);
     for (int answer: answers) {
         std::cout << answer << "\n";
     }
